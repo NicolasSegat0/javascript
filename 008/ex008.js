0762f9cf6159d777a9b4b8301a4b7d95d86debc7
Nicolas Navas Segato <ninavassegato@gmail.com>
2022-04-04 17:08:49 -0300

    novo

---
+++ 008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`A velocidae do seu carro é ${vel}km/h`)
+if (vel > 60){
+  console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
